520d6dddd7d4f076d2da0c25e17baa8cf8db18ee
Oveatrint <kbaiyewu@gmail.com>
2026-04-20 10:35:14 +0100

    removed postgres details :)

--- djangoORMtest/setupdjangoORM.py
+++ djangoORMtest/setupdjangoORM.py
@@ -1,20 +1,7 @@
+import os
+
 import django
 from django.conf import settings
 
-settings.configure(
-    DATABASES={
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "blogs",
-            "USER": "postgres",
-            "PASSWORD": "kamal265",
-            "HOST": "localhost",
-            "PORT": "5432",
-        }
-    },
-    INSTALLED_APPS=[
-        "blogs",
-    ],
-)
-
+os.environ["DJANGO_SETTINGS_MODULE"] = "settings"
 django.setup()
